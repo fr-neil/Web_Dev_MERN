c5a447e22da0a15a1aed28290f463c0ca9490779
Nil-Ajay <nileshaher141@gmail.com>
2021-08-13 13:49:38 +0530

    assignment done!

---
+++ Java-Script/js course/01- JS Fundamentals/assignments/02_data types/script.js
@@ -0,0 +1,9 @@
+var country = 'india';
+var isIsland = false;
+var launguage;
+var continent = 'asia';
+var population = '1.35B'
+console.log(typeof country)
+console.log(typeof  isIsland)
+console.log(typeof population)
+console.log(typeof launguage)
