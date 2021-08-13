d8a4bd6d51a5ae9011b2b55e7b50dd7c22e70da5
Nil-Ajay <nileshaher141@gmail.com>
2021-08-13 15:07:10 +0530

    1st challenge done!

---
+++ Java-Script/js course/01- JS Fundamentals/coding challenges/JS fundamentals01/coding challenge#1/script.js
@@ -0,0 +1,29 @@
+// TEST DATA ONE
+// let weightMark = 78;
+// let heightMark = 1.69;
+
+// let weightJohn = 92;
+// let heightJohn = 1.76;
+
+// let bmiMark = weightMark / heightMark ** 2;
+// console.log('mark\'s BMI: ' + bmiMark)
+// let bmiJohn = weightJohn / heightJohn ** 2;
+// console.log('John\'s BMI: ' + bmiJohn)
+
+// let markHigherBmi = bmiMark > bmiJohn;
+// console.log(markHigherBmi)
+
+//TEST DATA TWO
+let weightMark = 95;
+let heightMark = 1.88;
+
+let weightJohn = 85;
+let heightJohn = 1.76;
+
+let bmiMark = weightMark / heightMark ** 2;
+console.log('mark\'s BMI: ' + bmiMark)
+let bmiJohn = weightJohn / heightJohn ** 2;
+console.log('John\'s BMI: ' + bmiJohn)
+
+let markHigherBmi = bmiMark > bmiJohn;
+console.log(markHigherBmi)
