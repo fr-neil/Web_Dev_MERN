00fdc82c2c2e1c4400cf5310a9c6da0787ca424f
Nil-Ajay <nileshaher141@gmail.com>
2021-08-13 14:31:36 +0530

    assignment done!

---
+++ Java-Script/js course/01- JS Fundamentals/assignments/04_basic poerators/script.js
@@ -0,0 +1,21 @@
+const country = 'india';
+const isIsland = false;
+const launguage = 'Marathi';
+const continent = 'asia';
+let population = 1350000000;
+
+
+let partCountry = population / 2;
+console.log('population lives in each half: ' + partCountry);
+
+population++
+console.log(population)
+
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation)
+
+let averagePopulation = 33000000;
+console.log(averagePopulation > population)
+
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+console.log(description);
