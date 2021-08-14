725bfa84578d28cfcf1ffc1f6e722a7d6d71ff4c
Nil-Ajay <nileshaher141@gmail.com>
2021-08-14 20:38:56 +0530

    Coding challenge done!

---
+++ Java-Script/js course/01- JS Fundamentals/coding challenges/JS fundamentals01/coding challenge#3/script.js
@@ -0,0 +1,47 @@
+// data one
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(`Final average score of Dolphins : ${scoreDolphins.toFixed(3)}
+// Final average score of koalas : ${scoreKoalas.toFixed(3)} `);
+
+// if (scoreDolphins > 100 && scoreDolphins > scoreKoalas){
+//     console.log(`Dolphins wins the match!`)
+// }else if(scoreDolphins > 100){
+//     console.log(`Koalas wins the match!`)
+// }else if (scoreDolphins === scoreKoalas){
+//     console.log(`Match has been drwan!`)
+// }else{
+//     console.log(`No one's eligible to win trophy!`)
+// }
+
+// data two
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 123) / 3;
+// console.log(`Final average score of Dolphins : ${scoreDolphins.toFixed(3)}
+// Final average score of koalas : ${scoreKoalas.toFixed(3)} `);
+
+// if (scoreDolphins > 100 && scoreDolphins > scoreKoalas){
+//     console.log(`Dolphins wins the match!`)
+// }else if(scoreDolphins > 100){
+//     console.log(`Koalas wins the match!`)
+// }else if (scoreDolphins === scoreKoalas){
+//     console.log(`Match has been drwan!`)
+// }else{
+//     console.log(`No one's eligible to win trophy!`)
+// }
+
+// data three
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 123) / 3;
+console.log(`Final average score of Dolphins : ${scoreDolphins.toFixed(3)}
+Final average score of koalas : ${scoreKoalas.toFixed(3)} `);
+
+if (scoreDolphins >= 100 && scoreDolphins > scoreKoalas){
+    console.log(`Dolphins wins the match!`)
+}else if(scoreKoalas >= 100 && scoreKoalas > scoreDolphins){
+    console.log(`Koalas wins the match!`)
+}else if (scoreDolphins === scoreKoalas){
+    console.log(`Match has been drwan!`)
+}else{
+    console.log(`No one's eligible to win trophy!`)
+}
