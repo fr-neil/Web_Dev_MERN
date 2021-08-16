3f2e02e28646b92244d7327363e9f33e3388084d
Nil-Ajay <nileshaher141@gmail.com>
2021-08-16 22:52:44 +0530

    assignment done!

---
+++ Java-Script/js course/02_ JS Fundamentals-2/assignments/06_introduction to objects/script.js
@@ -0,0 +1,12 @@
+// Create an object called 'myCountry' for a country of your choice, containing 
+// properties 'country', 'capital', 'language', 'population' and 
+// 'neighbours' (an array like we used in previous assignments)
+
+const myCountry = {
+    country: `india`,
+    capital: `New delhi`,
+    launguage: `Hindi`,
+    population: `1030`,
+    neighbours: `[sri lanka, pakistan, nepal, bangladesh, china, bhutan, myanmar,]`
+}
+console.log(myCountry[`neighbours`]);
