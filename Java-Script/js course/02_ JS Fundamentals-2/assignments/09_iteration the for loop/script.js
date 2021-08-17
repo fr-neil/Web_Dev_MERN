3f6314b0a9b180e5b9653d305ab52c3e455cc1d8
Nil-Ajay <nileshaher141@gmail.com>
2021-08-17 16:21:39 +0530

    assignment done!

---
+++ Java-Script/js course/02_ JS Fundamentals-2/assignments/09_iteration the for loop/script.js
@@ -0,0 +1,6 @@
+// 1. There are elections in your country! In a small town, there are only 50 voters. 
+// Use a for loop to simulate the 50 people voting, by logging a string like this to 
+// the console (for numbers 1 to 50): 'Voter number 1 is currently voting' 
+for (let voters = 1; voters <= 50; voters++){
+    console.log(`voter number ${voters} is currently voting`);
+}
