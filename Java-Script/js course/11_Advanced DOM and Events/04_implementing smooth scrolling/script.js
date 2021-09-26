1110939825f01133d18a3a244922de531b8d91e9
Nil-Ajay <nileshaher141@gmail.com>
2021-09-26 15:46:47 +0530

    added smooth scrool to site

--- Java-Script/js course/11_Advanced DOM and Events/04_implementing smooth scrolling/script.js
+++ Java-Script/js course/11_Advanced DOM and Events/04_implementing smooth scrolling/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
 
 const openModal = function (e) {
   e.preventDefault();
@@ -47,35 +49,6 @@ document
   });
 message.style.backgroundColor = "#37383d";
 
-// 1 ==> Styles
-// to get all styles appliad to an element we use
-// console.log(getComputedStyle(message));
-// it listes all styles to applied to an element
-
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 18 + "px";
-
-// working with costom js properties
-// root element is equivalent to document element in js
-
-// here, document is itself root element, and with setProperty we can change default value of our costom property to all instances
-document.documentElement.style.setProperty("--color-primary", "orange");
-
-// 2 ==> Attributes
-// src, alt, class, id, href, and so on are all the attributes of an element
-// with js we can change this attributes
-
-const logo = document.querySelector(`.nav__logo`);
-
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
-
-// changing value
-logo.alt = "Beautiful minimalist logo";
-
-// Classes ==>
-logo.classList.add();
-logo.classList.remove();
-logo.classList.toggle();
-logo.classList.contains();
+btnScrollTo.addEventListener(`click`, function () {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
