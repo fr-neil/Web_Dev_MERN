dd53461d01cab89f8131be446307f1fc0179795d
Nil-Ajay <nileshaher141@gmail.com>
2022-02-13 07:47:36 +0530

    parcel and npm scripts

---
+++ Java-Script/js course/15_Modern JavaScript ES6/04_bundling with parcel and npm scripts/script.js
@@ -0,0 +1,35 @@
+const shoppingCart2 = (function (product, quantity) {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 260;
+  const totalQuantity = 10;
+
+  const addCart = (product, quantity) => {
+    cart.push({ product, quantity });
+    console.log(`Added ${quantity} ${product} to cart`);
+  };
+
+  const orderSummary = function (product, quantity) {
+    console.log(`Ordered ${quantity} ${product} from supplier`);
+  };
+
+  return {
+    addCart,
+    orderSummary,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addCart(`apple`, 2);
+shoppingCart2.orderSummary(`orange`, 2);
+
+// this all works because of closure
+
+if (module.hot) {
+  module.hot.accept();
+}
+// this is for hot reloading, it'll prevent current browser state from being lost when you reload the page
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
