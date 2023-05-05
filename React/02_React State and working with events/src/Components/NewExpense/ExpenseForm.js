f9be3154a1eed526274ba00ddb40ad2db5d25b26
Nil-Ajay <nileshaher141@gmail.com>
2023-05-05 08:09:33 +0530

    Using multiple ways to handle state

--- React/02_React State and working with events/src/Components/NewExpense/ExpenseForm.js
+++ React/02_React State and working with events/src/Components/NewExpense/ExpenseForm.js
@@ -4,19 +4,69 @@ import './ExpenseForm.css'
 
 const ExpenseForm = () => {
 
-    const [enteredTitle, setEnteredTitle] = React.useState('');
-    const [enteredAmount, setEnteredAmount] = React.useState('');
-    const [enteredDate, setEnteredDate] = React.useState('');
+    // this can be used as well but there is one more way to handle more than one state in our application
+    // const [enteredTitle, setEnteredTitle] = React.useState('');
+    // const [enteredAmount, setEnteredAmount] = React.useState('');
+    // const [enteredDate, setEnteredDate] = React.useState('');
+    // const titleChangeHandler = (e) => {
+    //     setEnteredTitle(e.target.value)
+    // }
+    // const amountChangeHandler = (e) => {
+    //     setEnteredAmount(e.target.value)
+    // }
+    // const dateChangeHandler = (e) => {
+    //     setEnteredDate(e.target.value)
+    // }
+
+
+    // Both of these state handlers can be used
+    // more people in industry prefer the one above, which is the seperate useState handling
+    const [userInput, setUserInput] = useState({
+        enterdTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    })
+
     const titleChangeHandler = (e) => {
-        setEnteredTitle(e.target.value)
-    }
-    const amountChangeHandler = (e) => {
-        setEnteredAmount(e.target.value)
-    }
-    const dateChangeHandler = (e) => {
-        setEnteredDate(e.target.value)
+        // As we know, react schedules a re-rendering of the component, using following approach we always get the      previousState snapshot, just to update one value, which in many cases can give us outdated state
+        // setUserInput({
+        //     ...userInput,
+        //     enterdTitle: e.target.value
+        // })
+
+        // Safest way to ensure we always work on latest state snapshot
+        setUserInput = (prevState) => {
+            return {
+                ...prevState,
+                enteredTitle: e.target.value
+            }
+        }
     }
 
+    const amountChangeHandler = (e) => {
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: e.target.value
+        // })
+        setUserInput = (prevState) => {
+            return {
+                ...prevState,
+                enteredAmount: e.target.value
+            }
+        }
+    }
+    const dateChangeHandler = (e) => {
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: e.target.value
+        // })
+        setUserInput = (prevState) => {
+            return {
+                ...prevState,
+                enteredDate: e.target.value
+            }
+        }
+    }
     return (
         <div>
             <form>
