6e7b7a50338a0501113d11cec51892d8be457d96
Nil-Ajay <nileshaher141@gmail.com>
2023-05-05 23:09:58 +0530

    working with diff types of sending responses

--- Node/app.js
+++ Node/app.js
@@ -1,28 +1,35 @@
-//imports the built-in Node.js http module, which provides an API for creating HTTP servers and making HTTP requests.
+// // Traditional way of sending responses
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+
+//     // res.setHeader() is used to set a single HTTP response header. It takes two arguments: the name of the header, and the value of the header. For example:
+//     res.setHeader('Content-Type', 'text/html')
+
+//     // The res.write() method is used to write data to the response stream in chunks. You can call it multiple times with smaller pieces of data and it will be sent to the client as soon as possible. It returns true if the data was successfully buffered and false otherwise, indicating that the stream has ended and no more data can be written.
+//     res.write('<html>')
+//     res.write('<head><title>My First Page</title></head>')
+//     res.write('<body><h1>Hello from my nodejs server!</h1></body>')
+//     res.write('</html>')
+
+//     // The res.end() method, on the other hand, is used to end the response stream and send the response to the client. If you provide data as an argument to res.end(), it will be sent to the client as the final chunk of data in the response. After calling res.end(), no more data can be written to the response stream.
+//     res.end()
+// })
+// server.listen(3000)
+
+
+// More efficient way of sending responses
+
 const http = require('http');
 
-// This line creates a new HTTP server using the http.createServer() method and passes in a callback function that will be executed each time a new HTTP request is received by the server. The callback function takes two arguments, req and res, representing the incoming request and the server's response to the request, respectively.
 const server = http.createServer((req, res) => {
 
-    // This line sets the HTTP status code of the response to 200, which means "OK". This indicates that the server has successfully handled the request and is sending back a valid response.
-    res.statusCode = 200
+    // res.writeHead() is used to set the response status code, response headers, and the HTTP version in a single call. It takes two arguments: the status code, and an object containing the response headers. For example:
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello welcome to my nodejs server!</h1>');
+});
 
-    // This line sets the value of the Content-Type header of the response to text/plain. This header tells the client what type of content is being sent in the response, in this case, plain text.
-    res.setHeader('Content-Type', 'text/plain')
-
-    // This line sends the response body to the client. In this case, it sends the string 'Hello, world!' as the response body.
-    res.end('Hello World')
-})
-
-// This line starts the server listening on port 3000. The second argument to server.listen() is a callback function that will be executed once the server has started listening for incoming requests.
 server.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+    console.log('Server running on port 3000');
+});
 
-
-// there are many core modules that nodeJs gives use, some of them are - http, https, fs, path, os
-// whenever we call a module in nodeJs we do it using require function, for code modules only name can be enough
-// for local modules we can use './app.js' as we always do
-
-// req: represents the HTTP request that the server has received from the client. It contains information about the request, such as the URL, request method, headers, and any data that may be included in the request body.
-// res: represents the HTTP response that the server will send back to the client. It allows you to set response headers, write response data, and end the response.
